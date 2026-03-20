43613c1bd1da8c29c6aadb6c5d702115ee8d6d8d
KOSTUBH1197 <KOSTUBH1197>
2026-03-20 21:13:12 +0530

    fix api url

--- frontend/utils/api.js
+++ frontend/utils/api.js
@@ -1,3 +1,13 @@
-const BASE_URL = "https://gigarmor-backend.onrender.com";
+const API = process.env.NEXT_PUBLIC_API_URL;
 
-export default BASE_URL;
+export const registerUser = async (data) => {
+  const res = await fetch(`${API}/api/auth/register`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+
+  return res.json();
+};
